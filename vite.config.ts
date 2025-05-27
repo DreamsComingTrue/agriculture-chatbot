6ad49ed60a275bdf0505eff8d5aaaa120dc416ec
Adam Ma <adamma2048@gmail.com>
2025-05-27 11:35:33 +0800

    fix: VITE_

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    // Pass environment variables to the app
-    'import.meta.env.OLLAMA_DOMAIN': JSON.stringify(process.env.OLLAMA_DOMAIN),
-  },
   plugins: [
     tailwindcss(),
     react()
@@ -18,9 +14,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  server: {
-    host: '0.0.0.0',  // Allow LAN/WAN access
-    port: 5173,
-    strictPort: true
-  },
+  // server: {
+  //   host: '0.0.0.0',  // Allow LAN/WAN access
+  //   port: 5173,
+  //   strictPort: true
+  // },
 })
